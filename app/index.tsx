0005ff3a706d18bf749e601f2005b99d29e11513
RicardoPLins <ricardinho200215@gmail.com>
2025-08-22 10:22:20 -0300

    second commit - set a standard colors

--- app/index.tsx
+++ app/index.tsx
@@ -4,8 +4,8 @@ import { Text, View } from "react-native";
 export default function Index() {
     return (
         <View className="flex-1 justify-center items-center bg-white">
-            <Text className="text-5xl text-blue-500 font-bold">Welcome!</Text>
-            <Text className="text-5xl text-red-800 font-bold">Red!</Text>
+            <Text className="text-5xl text-dark-200 font-bold">Welcome!</Text>
+            <Text className="text-5xl text-accent font-bold">Red!</Text>
             <Text className="text-3xl text-green-700 ">Finally, You get Green!</Text>
         </View>
     );
