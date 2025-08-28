f8d451035b722d5dcb4448af379cb515a102aa0d
RicardoPLins <ricardinho200215@gmail.com>
2025-08-28 11:02:43 -0300

    seventh commit - setup appwrite

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -7,6 +7,7 @@ import {fetchMovies} from "@/services/api";
 import MovieCard from "@/components/MovieCard";
 import {icons} from "@/constants/icons";
 import SearchBar from "@/components/SearchBar";
+import {updateSearchCount} from "@/services/appwrite";
 
 
 const Search = () => {
@@ -26,6 +27,10 @@ const Search = () => {
         const timeoutId = setTimeout(async () => {
             if (searchQuery.trim()) {
                 await loadMovies();
+
+                if(movies?.length>0 && movies?.[0])
+                await updateSearchCount(searchQuery, movies[0]);
+
             } else {
                 reset()
             }
--- services/appwrite.ts
+++ services/appwrite.ts
@@ -1,4 +1,4 @@
-import { Client, Databases, ID, Query } from "react-native-appwrite";
+import {Client, Databases, ID, Query} from "react-native-appwrite";
 
 const DATABASE_ID = process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID!;
 const COLLECTION_ID = process.env.EXPO_PUBLIC_APPWRITE_COLLECTION_ID!;
@@ -14,6 +14,7 @@ export const updateSearchCount = async (query: string, movie: Movie) => {
         const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
             Query.equal("searchTerm", query),
         ]);
+        console.log(result);
 
         if (result.documents.length > 0) {
             const existingMovie = result.documents[0];
@@ -26,7 +27,7 @@ export const updateSearchCount = async (query: string, movie: Movie) => {
                 }
             );
         } else {
-            await database.createDocument(DATABASE_ID, COLLECTION_ID, ID.unique(), {
+            await database.createDocument(DATABASE_ID, COLLECTION_ID, ID.unique(),{
                 searchTerm: query,
                 movie_id: movie.id,
                 title: movie.title,
