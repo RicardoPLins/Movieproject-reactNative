c0491f803920eb84a5500a5fa23c08a069cdbfe9
RicardoPLins <ricardinho200215@gmail.com>
2025-08-27 21:07:00 -0300

    sixth commit - create a search tab

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -18,8 +18,7 @@ export default function Index() {
         loading: moviesLoading,
         error: moviesError} = useFetch(() => fetchMovies({
         query: ''
-    }
-    ))
+    }))
 
 
     return (
--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -6,8 +6,10 @@ import {icons} from "@/constants/icons";
 interface Props{
     placeholder: string;
     onPress?: () => void;
+    value: string;
+    OnChangeText: (text: string) => void;
 }
-const SearchBar = ({ placeholder, onPress}: Props) => {
+const SearchBar = ({ placeholder, onPress, value, OnChangeText}: Props) => {
     return(
         <View className="flex-row items-center bg-dark-200
         rounded-full px-5 py-4">
@@ -16,8 +18,8 @@ const SearchBar = ({ placeholder, onPress}: Props) => {
             <TextInput
                 onPress={onPress}
             placeholder={placeholder}
-            value=""
-            onChangeText={() => {}}
+            value={value}
+            onChangeText={OnChangeText}
             placeholderTextColor="#a8b5db"
                 className="flex-1 ml2 text-white"
             />
